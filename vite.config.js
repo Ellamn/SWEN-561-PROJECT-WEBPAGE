a25a64486ed1cf7af8deca5ef88d29e4ab43bf91
Ella Natter <emcnatter@gmail.com>
2026-02-01 14:59:10 -0500

    config changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/swen-561-project-webpage/', // MUST match your GitHub repo name exactly
+  base: '/SWEN-561-PROJECT-WEBPAGE/', 
 })
